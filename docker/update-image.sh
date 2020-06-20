b2f661e96df2c959ef1ce1d3724f1999a5120388
Andrew Gunsch <andrew@gunsch.cc>
2020-06-20 16:34:35 -0700

    Adds initial config for gunsch.cc runtime.

---
+++ docker/update-image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+if [[ $# -lt 2 ]]; then
+  echo "Usage: $0 <repo-name> <image-version> [<branch-name>]";
+	echo ""
+  echo "    Example: $0 gunsch.cc 0.1.16"
+  echo "             restarts using docker.pkg.github.com/gunsch/<gunsch.cc>/master:<0.1.16>"
+  exit 1;
+fi
+
+if [[ "$(whoami)" != "root" ]]; then
+  echo "Must be run as root.";
+  exit 1;
+fi
+
+REPO_NAME=$1
+IMAGE_VERSION=$2
+BRANCH_NAME=${3:-master}
+
+GITHUB_PACKAGE_NAME="docker.pkg.github.com/gunsch/${REPO_NAME}/${BRANCH_NAME}:${IMAGE_VERSION}"
+
+docker pull "${GITHUB_PACKAGE_NAME}"
+docker tag "${GITHUB_PACKAGE_NAME}" "${REPO_NAME}:live"
+systemctl restart "docker-gunsch-${REPO_NAME}.service"
+
