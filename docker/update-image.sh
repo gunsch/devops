aad3121f1d543f6db7683b2d4785f68c6c88f986
Andrew Gunsch <andrew@gunsch.cc>
2020-06-22 22:04:07 -0700

    Adds GitHub deployments status

--- docker/update-image.sh
+++ docker/update-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -Eeuo pipefail
+set -Eeo pipefail
 if [[ $# -lt 2 ]]; then
   echo "Usage: $0 <repo-name> <image-version> [<branch-name>]";
 	echo ""
@@ -15,9 +15,41 @@ REPO_NAME=$1
 IMAGE_VERSION=$2
 BRANCH_NAME=${3:-master}
 
+github_description="Deploying ${BRANCH_NAME}:${IMAGE_VERSION}"
+
+function github_create_deployment {
+  if [[ -z "${GITHUB_DEPLOYMENT_SECRET}" ]]; then return; fi
+
+  # I'm lazy, assume success.
+  deployment_id=$(curl \
+      -H "Authorization: token ${GITHUB_DEPLOYMENT_SECRET}" \
+      -H "content-type: application/json" \
+      -d "{\"ref\":\"master\", \"description\": \"${github_description}\"}" \
+      "https://api.github.com/repos/gunsch/${REPO_NAME}/deployments" | \
+      python3 -c 'import json; import sys; print(json.load(sys.stdin)["id"])')
+  echo ${deployment_id}
+}
+
+function github_update_deployment {
+  if [[ -z "${GITHUB_DEPLOYMENT_SECRET}" ]]; then return; fi
+
+  deployment_id=$1
+  status=$2
+  curl \
+      -H "Authorization: token ${GITHUB_DEPLOYMENT_SECRET}" \
+      -H "content-type: application/json" \
+      -d "{\"state\": \"${status}\", \"description\": \"${github_description}\"}" \
+      "https://api.github.com/repos/gunsch/${REPO_NAME}/deployments/${deployment_id}/statuses"
+}
+
 GITHUB_PACKAGE_NAME="docker.pkg.github.com/gunsch/${REPO_NAME}/${BRANCH_NAME}:${IMAGE_VERSION}"
+
+# Start a GitHub deployment indicator
+deployment_id=$(github_create_deployment)
 
 docker pull "${GITHUB_PACKAGE_NAME}"
 docker tag "${GITHUB_PACKAGE_NAME}" "${REPO_NAME}:live"
 docker-compose -f "${DIR}/${REPO_NAME}/docker-compose.yml" restart
 
+# Update GitHub deployment as a success!
+github_update_deployment "${deployment_id}" "success"
